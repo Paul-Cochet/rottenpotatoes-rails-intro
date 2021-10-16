0df17e695427ebb8b3cc438007f757aa3ce2a4e1
Paul Cochet <paul.cochet@berkeley.edu>
2021-10-16 01:25:19 +0000

    part 1 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,14 @@
 class Movie < ActiveRecord::Base
   
-  def all_ratings()
-    return ['G','PG','PG-13','R']
+  def self.all_ratings()
+    return ['G','PG','PG-13','R','NC-17']
+  end
+  
+  def self.with_ratings(ratings)
+    if (ratings == nil)
+      Movie.all
+    else
+      Movie.where(rating: ratings.keys)
+    end
   end
 end
